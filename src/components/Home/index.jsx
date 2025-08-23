8f13e291125f6cffd37c5f2b480d4a8e352ca1dc
nandinierra <nandinierra34@gmail.com>
2025-08-23 14:36:30 +0530

    portfolio

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,7 +1,11 @@
 import './index.css'
+import {useNavigate} from "react-router"
 
 const Home=()=>{
-
+  const navigate=useNavigate()
+  const exploreMoreBtn=()=>{
+      navigate("/skills", {replace:true})
+  }
     return(
    <>
    <div className='container1'>
@@ -9,7 +13,7 @@ const Home=()=>{
             <div className='home-description'>
           <h1 className='home-container-h1'>Hii I'm Nandini Erra</h1>
             <p className='home-container-p'>Aspiring MERN developer, First-year at NIAT, Falling in love with tech, exploring infinite possibilities with code and creativity.</p>
-            <button>Explore More</button>
+            <button onClick={exploreMoreBtn}>Explore More</button>
             </div>
             <img src='https://res.cloudinary.com/doicvqkvb/image/upload/v1750734277/portfolio_profile_bf7e4c.jpg' />
     </div>
